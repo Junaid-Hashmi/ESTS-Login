cf2c160b75e24ae05f73228fa701a0197b861fea
Junaid-Hashmi7 <112497134+Junaid-Hashmi@users.noreply.github.com>
2022-10-08 20:32:03 +0530

    Confirm Password Check

--- script.js
+++ script.js
@@ -35,6 +35,19 @@ login.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
+// Confirm Password check
+function checkPwd() {
+  const pwd = document.getElementById("create-pwd").value;
+  const confirmPwd = document.getElementById("confirm-pwd").value;
+
+  if (pwd.length !== 0 && confirmPwd.length !== 0) {
+    if (pwd !== confirmPwd) {
+      alert("Error: Passwords don't match");
+    }
+  } else {
+    alert("Error: Password cannot be empty");
+  }
+}
 // File Chosen display
 uploadBtn.addEventListener("change", function () {
   fileChosen.textContent = this.files[0].name;
