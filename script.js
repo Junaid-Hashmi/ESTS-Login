ef2b5481f6046e7c003464f0e00d397fca7b5a69
Junaid-Hashmi7 <112497134+Junaid-Hashmi@users.noreply.github.com>
2022-10-09 20:28:31 +0530

    firebase-authentication

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector(".container");
 const pwdShowHide = document.querySelectorAll(".showHidePw");
 const pwFields = document.querySelectorAll(".password");
-const signUp = document.querySelector(".signup-link");
-const login = document.querySelector(".login-link");
+const signUpLink = document.querySelector(".signup-link");
+const loginLink = document.querySelector(".login-link");
 const uploadBtn = document.querySelector("#file-btn");
 const fileChosen = document.querySelector("#file-chosen");
 const signupBtn = document.querySelector(".signup-btn");
@@ -29,10 +29,10 @@ pwdShowHide.forEach((eyeIcon) => {
 });
 
 // displaying login <=> signup
-signUp.addEventListener("click", () => {
+signUpLink.addEventListener("click", () => {
   container.classList.add("active");
 });
-login.addEventListener("click", () => {
+loginLink.addEventListener("click", () => {
   container.classList.remove("active");
 });
 
@@ -53,3 +53,66 @@ signupBtn.addEventListener("click", () => {
 uploadBtn.addEventListener("change", function () {
   fileChosen.textContent = this.files[0].name;
 });
+
+// ----Authentication----
+
+// Setting up firebase with the page
+const firebaseApp = firebase.initializeApp({
+  /* Firebase config */
+  apiKey: "AIzaSyDl-KzVqzZFfTtp1cexjM5a6q20BpoySyM",
+  authDomain: "ests-login.firebaseapp.com",
+  projectId: "ests-login",
+  storageBucket: "ests-login.appspot.com",
+  messagingSenderId: "791910040853",
+  appId: "1:791910040853:web:55c625a5f8edaf90b3551c",
+});
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+// Signup function
+function signup() {
+  const email = document.getElementById("signup-email").value;
+  const password = document.getElementById("create-pwd").value;
+  console.log(email, password);
+  // firebase code
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log(userCredential);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+
+      // ..
+    });
+}
+
+// Login Function
+function login() {
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-pwd").value;
+  // firebase code
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log("Login successful");
+      console.log(userCredential);
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+}
